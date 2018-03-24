ae3f7f5755c768127334baf18b72b88207c71b09
mds <17163988+mds1@users.noreply.github.com>
2018-03-23 20:11:15 -0400

    Updated readme and contract files

---
+++ src/ethereum/lotteryGeneral.js
@@ -0,0 +1,21 @@
+import web3 from './web3'
+// ./build/Lottery.json will be output after compiling
+import Lottery from './build/Lottery.json'
+
+// This file is not used in this template and is just here for reference
+//
+// Create and export function which allows you to import this file,
+// then call the imported function with an address as the input to
+// access that instance of the contract, such as:
+//
+//    import Lottery from '@ethereum/Lottery'
+//    const lottery = Lottery(addressToContract)
+//
+// An example use case of this is a crowdfunding dapp, where the dapp has a
+// smart contract that allows user to generate their own crowdfunding campaign
+// smart contracts. Since each campaign viewable from the dapp has its
+// own contract, this function lets you specify the address of a specific
+// contract to access.
+export default (address) => {
+  return new web3.eth.Contract(JSON.parse(Lottery.interface), address)
+}
--- src/ethereum/lotteryInstance.js
+++ src/ethereum/lotteryInstance.js
@@ -1,11 +1,11 @@
 import web3 from './web3'
-// ./build/ContractName.json will be output after compiling
+// ./build/Lottery.json will be output after compiling
 import lottery from './build/lottery.json'
 
 // Create and export instance of the lottery contract
 // This allows us to simply import this file to access the deployed contract, e.g:
 //
-//    import lottery from root\ethereum\lotteryInstance.js
+//    import lottery from @ethereum\lotteryInstance.js
 //    const output = await lottery.methods.someMethod().call()
 const instance = new web3.eth.Contract(JSON.parse(lottery.interface),
   // after deployment, replace this address with the address of the deployed contract
