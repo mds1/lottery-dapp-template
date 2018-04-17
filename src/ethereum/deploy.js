d2c3a7ffdced8436aeff2cc6b91011d708767a19
mds <17163988+mds1@users.noreply.github.com>
2018-04-17 19:44:52 -0400

    Minor spacing updates

--- src/ethereum/deploy.js
+++ src/ethereum/deploy.js
@@ -4,9 +4,9 @@ const compiledLottery = require('./build/Lottery.json')
 
 // Set up provider for Rinkeby testnet
 const provider = new HDWalletProvider(
-  // this seed is a development-only account which holds no real Ether
+  // this seed is a dummy seed which holds no real Ether
   // replace this with the appropriate seed
-  'spend nephew bird brown rude kitten spice scale satisfy cruise mobile excess',
+  'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
   // replace this with your own infura address
   'https://rinkeby.infura.io/ExTa98BYpyL3Smnegw8u'
 )
@@ -15,7 +15,7 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider)
 
 // Create function to enable us to use async/await
-const deploy = async () => {
+const deploy = async() => {
   // Get list of accounts from seed phrase above
   const accounts = await web3.eth.getAccounts()
 
@@ -25,12 +25,12 @@ const deploy = async () => {
   // Create instance of contract, deploy it, and send it
   const result = await new web3.eth.Contract(JSON.parse(compiledLottery.interface))
     .deploy({
-      data: compiledLottery.bytecode
+      data: compiledLottery.bytecode,
       // arguments: ['Hi!']
     })
     .send({
       from: accounts[0],
-      gas: '1000000'
+      gas: '1000000',
     })
 
   // Get address of where contract was deployed to
