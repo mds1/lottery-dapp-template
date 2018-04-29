66dcce96daebe90f61d35d35f45af816af7729de
mds <17163988+mds1@users.noreply.github.com>
2018-04-29 18:39:37 -0400

    Small updates for displaying source code

--- src/pages/ContractSource.vue
+++ src/pages/ContractSource.vue
@@ -6,12 +6,19 @@
     <p>This smart contract is located at
       <a :href='this.addressURL' target='_blank'>{{ this.address }}</a>
     </p>
+
+    <pre>
+      <code>
+      {{ this.code }}
+      </code>
+    </pre>
+
   </div>
 </template>
 
 <script>
 import lottery from '@ethereum/lotteryInstance.js'
-import {createEtherscanAddressLink} from '@components/common/functions'
+import {createEtherscanAddressLink, getSourceCodeFromEtherscan} from '@components/common/functions'
 
 export default {
   data() {
@@ -23,15 +30,33 @@ export default {
   },
 
   created() {
-    // Generate transaction link
-    this.addressURL = createEtherscanAddressLink(this.$store.state.network.required, this.address)
+    // Get network and contract address
+    const network = this.$store.state.network.required
+    const address = this.address
 
-    // Read solidity contract
-    // const fr = new FileReader()
+    // Generate transaction link
+    this.addressURL = createEtherscanAddressLink(network, address)
+
+    // Get source code (this function is in progress)
+    this.code = getSourceCodeFromEtherscan(network, address)
   },
+
 }
 </script>
 
-<style>
+<style lang="stylus">
+pre, code {
+  // why "monospace, monospace"? see https://stackoverflow.com/questions/38781089/font-family-monospace-monospace
+  font-family: monospace, monospace;
+}
 
+code {
+  overflow: auto;
+}
+
+code {
+  display: block;
+  padding: 1rem;
+  word-wrap: normal;
+}
 </style>
