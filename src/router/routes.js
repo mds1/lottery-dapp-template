b5ccc4f65044dab0dc79cd735c7f8f32ef6b2544
mds <17163988+mds1@users.noreply.github.com>
2018-04-29 18:00:09 -0400

    Fixed wrong route for FAQ component

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,12 @@
 import Home from '@pages/Home.vue'
 import ContractSource from '@pages/ContractSource.vue'
+import FAQ from '@pages/FAQ.vue'
 import Error404 from '@pages/Error404.vue'
 
 export default [
   { path: '/', component: Home },
   { path: '/contract', component: ContractSource, name: 'contractSource' },
-  { path: '/faq', component: ContractSource, name: 'faq' },
+  { path: '/faq', component: FAQ, name: 'faq' },
 
   { // Always leave this as last one
     path: '*', component: Error404,
