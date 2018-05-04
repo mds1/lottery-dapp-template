dfb6ddd178a3b7eefd66e7f0d5fbf7040f6db8e8
mds <17163988+mds1@users.noreply.github.com>
2018-05-04 15:08:49 -0400

    Added new tests

--- src/ethereum/truffle/test/Lottery.test.js
+++ src/ethereum/truffle/test/Lottery.test.js
@@ -15,11 +15,12 @@ contract('Lottery', accounts => {
   // define variable to hold the instance of our Lottery.sol contract
   let lottery
 
-  // define function to enter lotter
+  // define function to enter lottery
   async function enterLottery(value, account) {
     // value: amount to send to contract, ether
     // account: account to entery lottery with
 
+    // send transaction
     const result = await lottery.enterLottery({
       from: account,
       value: web3.utils.toWei(String(value), 'ether'),
@@ -33,7 +34,7 @@ contract('Lottery', accounts => {
   })
 
   // check that it assigns the deployer as the manager
-  it('properly assigns the manager variable', async function() {
+  it('properly assigns the manager', async function() {
     assert.equal(await lottery.manager(), accounts[0], 'Variable "manager" was not properly set')
   })
 
@@ -44,7 +45,7 @@ contract('Lottery', accounts => {
   })
 
   // check that it won't allow people to enter who contribute less than the minimum
-  it('requires a minimum entry fee to enter', async function() {
+  it('requires a minimum entry fee to play', async function() {
     // minimum is set in Lottery.sol to 0.005
     try {
       await enterLottery(0.004, accounts[0]) // less than 0.005, should throw error
@@ -55,63 +56,82 @@ contract('Lottery', accounts => {
     assert(false, 'Lottery was successfully entered, but should have failed')
   })
 
-  // // check that it sends a query and receives a response
-  // it('sends a query and receives a response', async function() {
-  //   // for simplicity, we'll do both checks in this function
+  // end-to-end test -- tests for multiple entries and picking winner
+  it('allows multiple entries and picks a winner using Oraclize', async function() {
+    // set this test to timeout after 1 minute
+    this.timeout(60 * 1000)
 
-  //   // set this test to timeout after 1 minute
-  //   this.timeout(60 * 1000)
+    // have 5 people enter the lottery with entry fee of 1 ether
+    // already checked this function earlier so we won't test it again here
+    nplayers = 5 // number of players to simulate
+    entryFee = 1 // each player sends 1 ETH to enter
+    for (let i = 0; i < nplayers; i++) {
+      enterLottery(entryFee, accounts[i])
+    }
 
-  //   // enter the lottery
-  //   // make sure to send enough Ether and to set gas limit sufficiently high
-  //   const result = await lottery.getRandomNumber({
-  //     from: accounts[0],
-  //     value: web3.utils.toWei('0.000175', 'ether'),
-  //     gas: '3000000',
-  //   })
+    // begin process to pick winner
+    // make sure to send enough Ether and to set gas limit sufficiently high
+    const result = await lottery.getRandomNumber({
+      // value not needed to cover Orclize gas usage, since it's covered by entry fee
+      from: accounts[0],
+      gas: '3000000',
+    })
 
-  //   // Method 1 to check for events: loop through the "result" variable
+    // Method 1 to check for events: loop through the "result" variable
 
-  //   // look for the LogOraclizeQuery event to make sure query sent
-  //   let testPassed = false // variable to hold status of result
-  //   for (let i = 0; i < result.logs.length; i++) {
-  //     let log = result.logs[i]
-  //     if (log.event === 'LogOraclizeQuery') {
-  //       // we found the event
-  //       testPassed = true
-  //     }
-  //   }
-  //   assert(testPassed, '"LogOraclizeQuery" event not found')
+    // look for the LogOraclizeQuery event to make sure query sent
+    let testPassed = false // variable to hold status of result
+    for (let i = 0; i < result.logs.length; i++) {
+      let log = result.logs[i]
+      if (log.event === 'LogOraclizeQuery') {
+        // we found the event
+        testPassed = true
+      }
+    }
+    assert(testPassed, '"LogOraclizeQuery" event not found')
 
-  //   // Method 2 to check for events: listen for them with .watch()
+    // Method 2 to check for events: listen for them with .watch()
 
-  //   // listen for LogResultReceived event to check for Oraclize's call to _callback
-  //   // define events we want to listen for
-  //   const LogResultReceived = lottery.LogResultReceived()
+    // listen for LogResultReceived event to check for Oraclize's call to _callback
+    // define events we want to listen for
+    const LogResultReceived = lottery.LogResultReceived()
 
-  //   // create promise so Mocha waits for value to be returned
-  //   let checkForNumber = new Promise((resolve, reject) => {
-  //     // watch for our LogResultReceived event
-  //     LogResultReceived.watch(async function(error, result) {
-  //       if (error) {
-  //         reject(error)
-  //       }
-  //       // lottery.randomNumber() returns a BigNumber object
-  //       const bigNumber = await lottery.randomNumber()
-  //       // convert BigNumber to ordinary number
-  //       const randomNumber = bigNumber.toNumber()
-  //       // stop watching event and resolve promise
-  //       LogResultReceived.stopWatching()
-  //       resolve(randomNumber)
-  //     }) // end LogResultReceived.watch()
-  //   }) // end new Promise
+    // create promise so Mocha waits for value to be returned
+    let checkForNumber = new Promise((resolve, reject) => {
+      // watch for our LogResultReceived event
+      LogResultReceived.watch(async function(error, result) {
+        if (error) {
+          reject(error)
+        }
+        // lottery.winningNumber() returns a BigNumber object
+        const bigNumber = await lottery.winningNumber()
+        // convert BigNumber to ordinary number
+        const winningNumber = bigNumber.toNumber()
+        // stop watching event and resolve promise
+        LogResultReceived.stopWatching()
+        resolve(winningNumber)
+      }) // end LogResultReceived.watch()
+    }) // end new Promise
 
-  //   // call promise and wait for result
-  //   const randomNumber = await checkForNumber
-  //   // ensure result is within our query's min/max values
-  //   console.log(randomNumber)
-  //   assert.isAtLeast(randomNumber, 1, 'Random number was less than 1')
-  //   assert.isAtMost(randomNumber, 1000, 'Random number was greater than 1000')
+    // call promise and wait for result
+    const winningNumber = await checkForNumber
 
-  // }) // end 'it' block
+    // ensure resulting number is within the allowable range
+    assert.isAtLeast(winningNumber, 0,
+      `Random number generated (${winningNumber}) was less than 0`)
+    assert.isAtMost(winningNumber, nplayers,
+      `Random number generated (${winningNumber}) was greater than number of players (${nplayers})`)
+
+    // TODO: Add balance check. All balance checks currently return 0. Why??
+
+    // // get balance of winning account
+    // const winnersBalance = await web3.eth.getBalance(accounts[7])
+    // // approximate bound based on nplayers, entryFee, and default starting balance of 100 ETH
+    // // use 1 less player to account for gas
+    // const lowerBoundOfWinner = 100 + ((nplayers - 1) * entryFee)
+    // // ensure the winner's balance increased
+    // assert.isAtLeast(winnersBalance, lowerBoundOfWinner,
+    //   `Winning account had ${winnersBalance} ETH, but should have had at least ${lowerBoundOfWinner} ETH`)
+
+  }) // end 'it' block
 }) // end 'contract' block
